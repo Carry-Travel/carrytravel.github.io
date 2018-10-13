0c775b17e8937296ff2064b2f9959459b5ca791a
kashgupta <kashishg@wharton.upenn.edu>
2018-10-13 16:55:03 -0700

    added style to form

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_2CuQQDcWC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '44325nl0g06meq3a21g9c9ci58', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://e49s5ajyac.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
